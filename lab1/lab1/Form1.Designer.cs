c9be41c7a530b8b199e5d8fe7f56d1e92ae966a0
MaksymchukMaksymsettings <maksymchukmaxym@gmail.com>
2022-04-28 13:07:53 +0300

    add project of AVL-tree

---
+++ lab1/lab1/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.DataBindings.Add(new System.Windows.Forms.Binding("Name", global::lab1.Properties.Settings.Default, "я", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.treeView1.Location = new System.Drawing.Point(24, 12);
+            this.treeView1.Name = global::lab1.Properties.Settings.Default.я;
+            this.treeView1.Size = new System.Drawing.Size(621, 408);
+            this.treeView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(651, 404);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(879, 57);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(200, 100);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(938, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 26);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1141, 464);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    
+        
+    }
+}
+
---
+++ lab1/lab1/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+        static string[] text;
+        static int countWords = 0;
+        static int lenght;
+        public class Node
+        {
+           int count=0;
+            public string Value;
+            int height = 1;
+            public Node Father;
+            public Node Left;
+            public Node Right;
+
+            public void build()
+            {
+
+                
+                if (lenght > 1)
+                {
+                    if (Value == null)
+                    {
+                        Value = text[count];
+                    }
+                    Left = new Node();
+                    Right = new Node();
+                    Left.Value = text[count + 1];
+                    if ((lenght - 1) % 2 == 0)
+                    {
+                     Right.Value = text[(lenght/2)+1];
+                    }
+                    else
+                    {
+                        Right.Value = text[(lenght / 2) + 2];
+                    }
+                    count++;
+                    lenght /= 2;
+                    Left.build();
+                }
+
+                //Value = text[countWords];
+                //countWords++;
+                //if (this.Left == null)
+                //{
+                //    Left = new Node();
+                //    Left.Father = this;
+                //    if (Left.Father.Right != null)
+                //    {
+                //        this.Left.build();
+                //    }
+                //    else {
+                //        Right = new Node();
+                //        Right.Father = this;
+                //        this.Right.build();
+                //    }
+                //}
+                //else 
+                //{
+                //    Right = new Node();
+                //    Right.Father = this;
+                //    this.Right.build();
+                //}
+
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            //TreeNode tovarNode = new TreeNode("Товари");
+            //treeView1.Nodes.Add(tovarNode);
+            //tovarNode.Nodes.Add(new TreeNode("Смартфони"));
+
+            //TreeNode tovarNode2 = new TreeNode("Ресурси");
+            //tovarNode.Nodes.Add(tovarNode2);
+            //TreeNode tovarNode3 = new TreeNode("Повітр'я");
+            //treeView1.Nodes.Add(tovarNode3);
+            //label1.Text=tovarNode.NextNode.Text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ArrayText();
+            lenght = text.Length;
+            Node node = new Node();
+            node.build();
+        }
+        private void ArrayText() {
+            text=textBox1.Text.Split(new Char[] { ' ', ',', '.', ':', '\t' });
+           // label1.Text= text[1];
+        }
+    }
+}
